1a995f8e3321201878c4b0cf4cf7deec79fc6bb1
Roy Komprath <royk33776@yahoo.com>
2023-10-08 22:05:13 -0400

    commit-3

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,9 +1,15 @@
 "use client";
 import { AiOutlineMenu } from "react-icons/ai";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import Avatar from "../Avatar";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = useCallback(() => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  }, [isOpen]);
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
@@ -14,7 +20,7 @@ const UserMenu = () => {
           Airbnb your Home
         </div>
         <div
-          onClick={() => {}}
+          onClick={toggleOpen}
           className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
         >
           <AiOutlineMenu />
@@ -23,6 +29,26 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem
+                label="Login"
+                onClick={() => {
+                  console.log("Login");
+                }}
+              />
+              <MenuItem
+                label="Sign up"
+                onClick={() => {
+                  console.log("Sign up");
+                }}
+              />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
@@ -19,7 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Modal isOpen={true} />
+          <Navbar />
+        </ClientOnly>
+
         {children}
       </body>
     </html>
